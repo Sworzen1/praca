834f646e4efa33339c61ae34af92a93368fc8814
Bartlomiej Sworzen <b.sworzen@gmail.com>
2019-12-01 20:19:49 +0100

    add router to all components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Drawer from "./Drawer";
+import Profile from "./Profile";
+import Foods from "./Foods";
+import Contact from "./Contact";
+import Breakfast from "./Meals/Breakfast";
+import Lunch from "./Meals/Lunch";
+import Dinner from "./Meals/Dinner";
+import Snack from "./Meals/Snack";
+import Supper from "./Meals/Supper";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <div className={"app"}>
+        <Drawer />
+        <main className={"main"}>
+          <Switch>
+          <Route path="/supper">
+              <Supper />
+            </Route>
+            <Route path="/snack">
+              <Snack />
+            </Route>
+            <Route path="/dinner">
+              <Dinner />
+            </Route>
+            <Route path="/lunch">
+              <Lunch />
+            </Route>
+          <Route path="/breakfast">
+              <Breakfast />
+            </Route>
+            <Route path="/Foods">
+              <Foods />
+            </Route>
+            <Route path="/Contact">
+              <Contact />
+            </Route>
+            <Route path="/">
+              <Profile />
+            </Route>
 
+          </Switch>
+        </main>
+      </div>
+    </Router>
+  );
+};
 export default App;

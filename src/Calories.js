1e6455623e5beb20b09029bca8061b03e2f9c6c9
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-01-02 18:51:10 +0100

    set new login function

--- src/Calories.js
+++ src/Calories.js
@@ -1,55 +1,71 @@
-import React, { createContext, useState, useContext, useEffect } from 'react'
+import React, { createContext, useState, useContext, useEffect } from "react";
 
-const CaloriesContext = createContext()
+const CaloriesContext = createContext();
 
-const { Provider } = CaloriesContext
+const { Provider } = CaloriesContext;
 
-const CaloriesProvider = (props) => {
+const CaloriesProvider = props => {
+  const [login, setLogin] = useState(0);
+  const [carbo, setCarbo] = useState(0);
+  const [protein, setProtein] = useState(0);
+  const [fat, setFat] = useState(0);
+  const [calories, setCalories] = useState(0);
 
-    const [carbo, setCarbo] = useState(0)
-    const [protein, setProtein] = useState(0)
-    const [fat, setFat] = useState(0)
-    const [calories, setCalories] = useState(0)
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
-    const [havent, setRegister] = useState(false)
-    // useEffect(() => {
-    //     const caloriesFromMemory = localStorage.getItem('calories')
-    //     setCalories(parseInt(caloriesFromMemory))
-    // }, [])
+  // useEffect(() => {
+  //     const caloriesFromMemory = localStorage.getItem('calories')
+  //     setCalories(parseInt(caloriesFromMemory))
+  // }, [])
 
-    // useEffect(() => {
-    //     const carboFromMemory = localStorage.getItem('carbo')
-    //     // setCarbo(parseInt(carboFromMemory))
-    // }, [])
+  // useEffect(() => {
+  //     const carboFromMemory = localStorage.getItem('carbo')
+  //     // setCarbo(parseInt(carboFromMemory))
+  // }, [])
 
-    const addCalories = (carboToAdd,proteinToAdd, fatToAdd, caloriesToAdd) => {
-        
-        setCarbo(carbo + carboToAdd)
-        localStorage.setItem('carbo', carbo + carboToAdd)
+  const addCalories = (carboToAdd, proteinToAdd, fatToAdd, caloriesToAdd) => {
+    setCarbo(carbo + carboToAdd);
+    localStorage.setItem("carbo", carbo + carboToAdd);
 
-        setProtein(protein + proteinToAdd)
-        localStorage.setItem('protein', protein + proteinToAdd)
-        
-        setFat(fat + fatToAdd)
-        localStorage.setItem('fat', fat + fatToAdd)
+    setProtein(protein + proteinToAdd);
+    localStorage.setItem("protein", protein + proteinToAdd);
 
-        setCalories(calories + caloriesToAdd)
-        localStorage.setItem('calories', calories + caloriesToAdd)
-    }
-    const login = () => {
-        setIsLoggedIn(true)
-    }
-    const register = ()  => {
-        setRegister(true)
-    }
+    setFat(fat + fatToAdd);
+    localStorage.setItem("fat", fat + fatToAdd);
 
-    return <Provider value={{ protein, addCalories, login, isLoggedIn, carbo, fat, calories, register, havent}}>
-        {props.children}
+    setCalories(calories + caloriesToAdd);
+    localStorage.setItem("calories", calories + caloriesToAdd);
+  };
+
+  const LoggedIn = () => {
+    setLogin(2);
+  };
+  const Register = () => {
+    setLogin(1);
+  };
+  const Already = () => {
+    setLogin(0);
+  };
+
+  return (
+    <Provider
+      value={{
+        Already,
+        Register,
+        protein,
+        addCalories,
+        carbo,
+        fat,
+        calories,
+        login,
+        LoggedIn
+      }}
+    >
+      {props.children}
     </Provider>
-}
+  );
+};
 
 export const useCalories = () => {
-    return useContext(CaloriesContext)
-}
+  return useContext(CaloriesContext);
+};
 
-export default CaloriesProvider
+export default CaloriesProvider;

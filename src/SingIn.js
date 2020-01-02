a154eb4b960997e29149fc607cc6f7ad58f885fa
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-01-02 18:52:28 +0100

    change styles

--- src/SingIn.js
+++ src/SingIn.js
@@ -4,35 +4,35 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
-main:{
-    backgroundColor:"#17161a",
-    height:"100vh",
-    width:"100%",
-    color:"white",
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
-},
-formfield:{
-    marginBottom:"40px",
-},
-container:{
-    backgroundColor:"rgba(15,15,15,.8)",
-    width:"30%",
-    height:"50%",
+  main: {
+    backgroundColor: "#17161a",
+    height: "100vh",
+    width: "100%",
+    color: "white",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  formfield: {
+    marginBottom: "40px"
+  },
+  container: {
+    backgroundColor: "rgba(15,15,15,.8)",
+    width: "30%",
+    height: "50%",
     boxShadow: "15px 15px 5px 0px rgba(0,0,0,0.3)",
-    padding:"30px",
-},
-label: {
-    display:"block",
+    padding: "30px"
+  },
+  label: {
+    display: "block",
     textTransform: "uppercase",
     fontSize: ".9em",
-    color: "white",
+    color: "white"
   },
   input: {
     width: "85%",
     backgroundColor: "transparent",
-    border:"none",
+    border: "none",
     color: "white",
     outline: "none",
     borderBottom: "1px solid #f50057",
@@ -40,32 +40,33 @@ label: {
     fontWeight: "300",
     paddingBottom: "10px",
     marginTop: "10px",
+    '&:-webkit-autofill':{
+      transition:"background-color 5000s ease-in-out 0s",
+      WebkitTextFillColor:"white",
+      WebkitBoxShadow:"0 0 0px 1000px transparent inset"
+    }
   },
-  link:{
-    color: "#66707D",
+  link: {
+    cursor: "pointer",
     textDecoration: "none",
     display: "inline-block",
     borderBottom: "1.5px solid #f50057",
     paddingBottom: "5px",
-    marginLeft:"30px"
+    marginLeft: "30px"
   },
-  title:{
-      marginBottom:"50px",
+  title: {
+    marginBottom: "50px"
   }
-
-
-
 }));
 
 const SingIn = () => {
-  const { login } = useCalories();
+  const { Register, LoggedIn } = useCalories();
   const classes = useStyles();
   return (
     <div className={classes.main}>
       <div className={classes.container}>
+        <h2 className={classes.title}>SingIn</h2>
 
-<h2 className={classes.title}>SingIn</h2>
-        
         <div className={classes.formfield}>
           <label className={classes.label} htmlFor="email">
             E-Mail Adress
@@ -93,11 +94,18 @@ const SingIn = () => {
         </div>
 
         <div>
-        <Button size="large" color="secondary" onClick={login} className={classes.button}>
+          <Button
+            size="large"
+            color="secondary"
+            onClick={LoggedIn}
+            className={classes.button}
+          >
             Login
-          </Button> 
-          
-          <div className={classes.link}>Create your account</div>
+          </Button>
+
+          <div onClick={Register} className={classes.link}>
+            Create your account
+          </div>
         </div>
       </div>
     </div>
--- src/SingUp.js
+++ src/SingUp.js
@@ -4,35 +4,36 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
-main:{
-    backgroundColor:"#17161a",
-    height:"100vh",
-    width:"100%",
-    color:"white",
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
-},
-formfield:{
-    marginBottom:"40px",
-},
-container:{
-    backgroundColor:"rgba(15,15,15,.8)",
-    width:"30%",
-    height:"65%",
+  main: {
+    backgroundColor: "#17161a",
+    height: "100vh",
+    width: "100%",
+    color: "white",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  formfield: {
+    marginBottom: "40px"
+  },
+  container: {
+    backgroundColor: "rgba(15,15,15,.8)",
+    width: "30%",
+    height: "65%",
     boxShadow: "15px 15px 5px 0px rgba(0,0,0,0.3)",
-    padding:"30px",
-},
-label: {
-    display:"block",
+    padding: "30px"
+  },
+  label: {
+    display: "block",
     textTransform: "uppercase",
     fontSize: ".9em",
     color: "white",
+    
   },
   input: {
     width: "85%",
     backgroundColor: "transparent",
-    border:"none",
+    border: "none",
     color: "white",
     outline: "none",
     borderBottom: "1px solid #f50057",
@@ -40,33 +41,34 @@ label: {
     fontWeight: "300",
     paddingBottom: "10px",
     marginTop: "10px",
+    '&:-webkit-autofill':{
+      transition:"background-color 5000s ease-in-out 0s",
+      WebkitTextFillColor:"white",
+      WebkitBoxShadow:"0 0 0px 1000px transparent inset"
+    }
   },
-  link:{
-    color: "#66707D",
+  link: {
+    cursor: "pointer",
     textDecoration: "none",
     display: "inline-block",
     borderBottom: "1.5px solid #f50057",
     paddingBottom: "5px",
-    marginLeft:"30px"
+    marginLeft: "30px"
   },
-  title:{
-      marginBottom:"50px",
+  title: {
+    marginBottom: "50px"
   }
-
-
-
 }));
 
 const SingUp = () => {
-  const { register } = useCalories();
+  const { Already } = useCalories();
   const classes = useStyles();
   return (
     <div className={classes.main}>
       <div className={classes.container}>
+        <h2 className={classes.title}>SingUp</h2>
 
-<h2 className={classes.title}>SingUp</h2>
-
-<div className={classes.formfield}>
+        <div className={classes.formfield}>
           <label className={classes.label} htmlFor="name">
             FullName
           </label>
@@ -78,7 +80,7 @@ const SingUp = () => {
             name="name"
           ></input>
         </div>
-        
+
         <div className={classes.formfield}>
           <label className={classes.label} htmlFor="email">
             E-Mail Adress
@@ -106,16 +108,17 @@ const SingUp = () => {
         </div>
 
         <div>
-        <Button size="large" color="secondary" onClick={register} className={classes.button}>
+          <Button size="large" color="secondary" className={classes.button}>
             Register
-          </Button> 
-          
-          <div className={classes.link}>
-I already have an account</div>
+          </Button>
+
+          <div onClick={Already} className={classes.link}>
+            I already have an account
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default SingUp
+export default SingUp;

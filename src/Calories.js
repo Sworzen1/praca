20bac7da9832b6d1060a63f42de9fabe3b09c1a6
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-01-04 18:02:15 +0100

    create useEffects for specyfies

--- src/Calories.js
+++ src/Calories.js
@@ -1,27 +1,73 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
-
 const CaloriesContext = createContext();
 
 const { Provider } = CaloriesContext;
 
 const CaloriesProvider = props => {
+  const initialName = localStorage.getItem("name");
+  const initialAge = localStorage.getItem("age");
+  const initialWeight = localStorage.getItem("weight");
+  const initialHeight = localStorage.getItem("height");
+
   const [login, setLogin] = useState(0);
   const [carbo, setCarbo] = useState(0);
   const [protein, setProtein] = useState(0);
   const [fat, setFat] = useState(0);
   const [calories, setCalories] = useState(0);
   const [darkmode, setDark] = useState(true);
+  const [name, setName] = useState(initialName);
+  const [age, setAge] = useState(initialAge);
+  const [weight, setWeight] = useState(initialWeight);
+  const [height, setHeight] = useState(initialHeight);
 
-  // useEffect(() => {
-  //     const caloriesFromMemory = localStorage.getItem('calories')
-  //     setCalories(parseInt(caloriesFromMemory))
-  // }, [])
 
-  // useEffect(() => {
-  //     const carboFromMemory = localStorage.getItem('carbo')
-  //     // setCarbo(parseInt(carboFromMemory))
-  // }, [])
+  const addName = titleName => {
+    setName(titleName);
+  };
 
+  const addAge = titleAge => {
+    setAge(titleAge);
+  };
+  const addWeight = titleWeight => {
+    setWeight(titleWeight);
+  };
+
+  const addHeight = titleHeight => {
+    setHeight(titleHeight);
+  };
+
+
+  useEffect(() => {
+    localStorage.setItem("age", age);
+  }, [age]);
+
+  useEffect(() => {
+    localStorage.setItem("name", name);
+  }, [name]);
+  useEffect(() => {
+    localStorage.setItem("weight", weight);
+  }, [weight]);
+  useEffect(() => {
+    localStorage.setItem("height", height);
+  }, [height]);
+
+  useEffect(() => {
+    const carboFromMemory = localStorage.getItem("carbo");
+    setCarbo(parseInt(carboFromMemory));
+  }, []);
+  useEffect(() => {
+    const proteinFromMemory = localStorage.getItem("protein");
+    setProtein(parseInt(proteinFromMemory));
+  }, []);
+  useEffect(() => {
+    const fatFromMemory = localStorage.getItem("fat");
+    setFat(parseInt(fatFromMemory));
+  }, []);
+  useEffect(() => {
+    const caloriesFromMemory = localStorage.getItem("calories");
+    setCalories(parseInt(caloriesFromMemory));
+  }, []);
+  
   const addCalories = (carboToAdd, proteinToAdd, fatToAdd, caloriesToAdd) => {
     setCarbo(carbo + carboToAdd);
     localStorage.setItem("carbo", carbo + carboToAdd);
@@ -36,7 +82,7 @@ const CaloriesProvider = props => {
     localStorage.setItem("calories", calories + caloriesToAdd);
   };
   const Dark = () => {
-setDark(!darkmode);
+    setDark(!darkmode);
   };
 
   const LoggedIn = () => {
@@ -62,7 +108,15 @@ setDark(!darkmode);
         login,
         LoggedIn,
         darkmode,
-        Dark
+        Dark,
+        addName,
+        name,
+        addAge,
+        age,
+        addHeight,
+        height,
+        addWeight,
+        weight
       }}
     >
       {props.children}
